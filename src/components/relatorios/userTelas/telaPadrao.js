f0ca020f4dbe4619c729238267d79509994420de
Leandro <andomonte@gmail.com>
2021-04-05 16:46:39 -0400

    v0.21

--- src/components/relatorios/userTelas/telaPadrao.js
+++ src/components/relatorios/userTelas/telaPadrao.js
@@ -1,15 +1,4 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-
-    justifyContent: 'center',
-  },
-}));
 
 export default function NestedGrid() {
   // const classes = useStyles();
