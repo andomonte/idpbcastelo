ca4aaa45e0e2153881b837290b94f07014a32e50
andomonte <andomonte@gmail.com>
2021-06-08 12:50:25 -0400

    v1.2

--- next.config.js
+++ next.config.js
@@ -1,18 +1,9 @@
 // next.config.js
 const withPlugins = require('next-compose-plugins');
-const withSvgr = require('next-svgr');
+
 const withPWA = require('next-pwa');
 
 module.exports = withPlugins([
-  {
-    env: {
-      MYSQL_HOST: 'mysql669.umbler.com',
-      MYSQL_PORT: '41890',
-      MYSQL_DATABASE: 'db_idpb',
-      MYSQL_USER: 'idpbsystem',
-      MYSQL_PASSWORD: 'idpbMM981341',
-    },
-  },
   {
     images: {
       domains: ['storage.googleapis.com'],
@@ -24,8 +15,11 @@ module.exports = withPlugins([
       ignoreBuildErrors: true,
     },
     env: {
-      SERVER_URL: 'https://my-json-server.typicode.com/solrachix/podcastr',
-      NEXT_PUBLIC_SITE_URL: 'http://localhost:3000',
+      MYSQL_HOST: 'mysql669.umbler.com',
+      MYSQL_PORT: '41890',
+      MYSQL_DATABASE: 'db_idpb',
+      MYSQL_USER: 'idpbsystem',
+      MYSQL_PASSWORD: 'idpbMM981341',
     },
   },
   [
@@ -39,6 +33,6 @@ module.exports = withPlugins([
       },
     },
   ],
-  withSvgr,
+
   // your other plugins here
 ]);
