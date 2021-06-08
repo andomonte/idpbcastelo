6d93a48f999fb0cf820fbb6eaa427fb063b62cae
andomonte <andomonte@gmail.com>
2021-06-08 13:27:13 -0400

    v1.2

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
-// next.config.js
-const withPlugins = require('next-compose-plugins');
+module.exports = {
+  env: {
+    MYSQL_HOST: 'mysql669.umbler.com',
+    MYSQL_PORT: '41890',
+    MYSQL_DATABASE: 'db_idpb',
+    MYSQL_USER: 'idpbsystem',
+    MYSQL_PASSWORD: 'idpbMM981341',
+  },
+};
+
+/* // next.config.js
+
+    const withPlugins = require('next-compose-plugins');
 
 const withPWA = require('next-pwa');
 
@@ -36,3 +47,4 @@ module.exports = withPlugins([
 
   // your other plugins here
 ]);
+ */
