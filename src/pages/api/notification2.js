e4f132ae8f60cbb686d7ececb3eced6311d152fc
andomonte <andomonte@gmail.com>
2022-01-05 08:27:53 -0400

    v2.65

--- src/pages/api/notification2.js
+++ src/pages/api/notification2.js
@@ -54,42 +54,26 @@ const Notificacao = async (req, res) => {
   }
   console.log('id--', id);
   if (mercadoPago && mercadoPago.response.status) {
-    const posts = await prisma.inscritosGlobals
-
-      .findMany({
-        where: {
-          AND: [{ idPagamento: id }],
-        },
-      })
-      .finally(async () => {
-        await prisma.$disconnect();
-      });
-    console.log(posts);
-    console.log('id--', id);
-
-    if (posts.length) {
-      try {
-        await prisma.inscritosGlobals
-          .update({
-            where: {
-              AND: [{ idPagamento: id }],
-            },
-            data: {
-              status: 'mercadoPagostatus',
-            },
-          })
-          .finally(async () => {
-            await prisma.$disconnect();
-          });
-
-        // res.send(JSON.stringify(respPagamento.status));
-        console.log('bd-idpb foi atualizado');
-        res.status(200).send('OK');
-      } catch (errors) {
-        //        const erroIDPB = JSON.stringify(ErroIDPB);
-        console.log('erro ao acessar bd-idpb=', errors);
-        res.status(400).send('vou criar o banco');
-      }
+    try {
+      const updateUser = await prisma.user
+        .update({
+          where: {
+            idPagamento: id,
+          },
+          data: {
+            status: 'mercadoPagostatus',
+          },
+        })
+        .finally(async () => {
+          await prisma.$disconnect();
+        });
+      // res.send(JSON.stringify(respPagamento.status));
+      console.log('bd-idpb foi atualizado', updateUser);
+      res.status(200).send('OK');
+    } catch (errors) {
+      //        const erroIDPB = JSON.stringify(ErroIDPB);
+      console.log('erro ao acessar bd-idpb=', errors);
+      res.status(400).send('vou criar o banco');
     }
   }
 };
