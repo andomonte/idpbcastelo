9931f9c4969ed4d69f0db8a5704f316e0922f261
andomonte <andomonte@gmail.com>
2022-04-08 00:45:07 -0400

    v0.02

--- src/components/castelo/logado/relatorios/perfil/meuPerfil.js
+++ src/components/castelo/logado/relatorios/perfil/meuPerfil.js
@@ -3,6 +3,10 @@ import { Box, Avatar } from '@material-ui/core';
 import React from 'react';
 import QRCode from 'react-qr-code';
 import corIgreja from 'src/utils/coresIgreja';
+import TamanhoJanela from 'src/utils/getSize';
+
+const janela = TamanhoJanela();
+console.log(janela);
 /* import useSWR from 'swr';
 
 function getDados(email, nome) {
@@ -21,194 +25,206 @@ function getDados(email, nome) {
 
 function meuPerfil({ secao, perfilUser }) {
   return (
-    <Box height="90vh" minHeight={500}>
+    <Box display="flex" justifyContent="center" alignItems="center">
       <Box
-        height="100%"
-        width="100vw"
-        minWidth={370}
-        mt={0}
         display="flex"
         justifyContent="center"
         alignItems="center"
+        height={janela.height - 56}
+        width="96vw"
+        minHeight={500}
       >
         <Box
-          minWidth={370}
           height="100%"
-          width="100vw"
-          maxWidth={600}
-          border="4px solid #fff"
+          width="100%"
+          mt={0}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
         >
           <Box
-            height="25%"
-            minHeight={150}
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            bgcolor={corIgreja.principal}
-            style={{
-              borderTopLeftRadius: '16px',
-              borderTopRightRadius: '16px',
-            }}
-          >
-            <Box>
-              <Box>
-                <Box display="flex" justifyContent="center" alignItems="center">
-                  <Avatar
-                    style={{
-                      width: 80,
-                      height: 80,
-                    }}
-                    alt="Remy Sharp"
-                    src="images/castelo/castelo.png"
-                  />
-                </Box>
-
-                <Box
-                  mt={2}
-                  sx={{
-                    fontFamily: 'Geneva',
-                    fontWeight: 'bold',
-                    fontSize: '22px',
-                    color: '#fff',
-                  }}
-                  textAlign="center"
-                >
-                  IDPB - CASTELO{' '}
-                </Box>
-              </Box>
-            </Box>
-          </Box>
-          <Box
-            height="73%"
-            minHeight={350}
-            bgcolor={corIgreja.principal}
-            borderTop="2px solid #fff"
-            style={{
-              borderBottomLeftRadius: '16px',
-              borderBottomRightRadius: '16px',
-            }}
+            minWidth={370}
+            height="100%"
+            width="100%"
+            maxWidth={600}
+            border="4px solid #fff"
           >
             <Box
-              height="40%"
-              minHeight={100}
-              display="flex"
-              justifyContent="center"
-              alignItems="center"
-              mt={1}
-            >
-              <QRCode
-                size={120}
-                value={perfilUser.Igreja + perfilUser.RolMembro}
-              />
-            </Box>
-
-            <Box
-              display="flex"
-              justifyContent="center"
-              alignItems="center"
-              height="15%"
-              minHeight={50}
-              textAlign="center"
-              border="2px solid #3f50b5"
-              sx={{
-                bgcolor: '#fff',
-                color: 'green',
-                fontFamily: 'Times New Roman Times serif',
-                fontWeight: 'bold',
-                fontSize: '28px',
-              }}
-            >
-              {secao.user.name}
-            </Box>
-            <Box
-              height="40%"
+              height="25%"
               minHeight={150}
               display="flex"
               justifyContent="center"
               alignItems="center"
+              bgcolor={corIgreja.principal}
+              style={{
+                borderTopLeftRadius: '16px',
+                borderTopRightRadius: '16px',
+              }}
             >
-              <Grid
-                container
-                direction="row"
+              <Box>
+                <Box>
+                  <Box
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                  >
+                    <Avatar
+                      style={{
+                        width: 80,
+                        height: 80,
+                      }}
+                      alt="Remy Sharp"
+                      src="images/castelo/castelo.png"
+                    />
+                  </Box>
+
+                  <Box
+                    mt={2}
+                    sx={{
+                      fontFamily: 'Geneva',
+                      fontWeight: 'bold',
+                      fontSize: '22px',
+                      color: '#fff',
+                    }}
+                    textAlign="center"
+                  >
+                    IDPB - CASTELO{' '}
+                  </Box>
+                </Box>
+              </Box>
+            </Box>
+            <Box
+              height="73%"
+              minHeight={350}
+              bgcolor={corIgreja.principal}
+              borderTop="2px solid #fff"
+              style={{
+                borderBottomLeftRadius: '16px',
+                borderBottomRightRadius: '16px',
+              }}
+            >
+              <Box
+                height="40%"
+                minHeight={100}
+                display="flex"
                 justifyContent="center"
-                alignItems="stretch"
+                alignItems="center"
+                mt={1}
               >
-                <Grid item xs={12} />
-                <Box
-                  width="100%"
-                  display="flex"
+                <QRCode
+                  size={120}
+                  value={perfilUser.Igreja + perfilUser.RolMembro}
+                />
+              </Box>
+
+              <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                height="15%"
+                minHeight={50}
+                textAlign="center"
+                border="2px solid #3f50b5"
+                sx={{
+                  bgcolor: '#fff',
+                  color: 'green',
+                  fontFamily: 'Times New Roman Times serif',
+                  fontWeight: 'bold',
+                  fontSize: '28px',
+                }}
+              >
+                {secao.user.name}
+              </Box>
+              <Box
+                height="40%"
+                minHeight={150}
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <Grid
+                  container
+                  direction="row"
                   justifyContent="center"
-                  alignItems="center"
-                  minHeight={150}
+                  alignItems="stretch"
                 >
-                  <Grid style={{ marginTop: 0 }} item xs={4}>
-                    <Box
-                      ml={4}
-                      display="flex"
-                      justifyContent="center"
-                      alignItems="center"
-                    >
-                      <Avatar
-                        style={{
-                          width: 100,
-                          height: 100,
-                        }}
-                        alt="Remy Sharp"
-                        src={secao.user.image}
-                      />
-                    </Box>
-                  </Grid>
+                  <Grid item xs={12} />
+                  <Box
+                    width="100%"
+                    display="flex"
+                    justifyContent="center"
+                    alignItems="center"
+                    minHeight={150}
+                  >
+                    <Grid style={{ marginTop: 0 }} item xs={4}>
+                      <Box
+                        ml={4}
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                      >
+                        <Avatar
+                          style={{
+                            width: 100,
+                            height: 100,
+                          }}
+                          alt="Remy Sharp"
+                          src={secao.user.image}
+                        />
+                      </Box>
+                    </Grid>
 
-                  <Grid item xs={8}>
-                    <Box
-                      ml={0}
-                      height="100%"
-                      display="flex"
-                      justifyContent="center"
-                      alignItems="center"
-                    >
-                      <Box>
-                        <Box
-                          sx={{
-                            color: '#fff',
-                            fontFamily: 'Times New Roman Times serif',
-                          }}
-                        >
-                          Funcão:
-                        </Box>
-                        <Box
-                          sx={{
-                            color: '#76ff03',
-                            fontFamily: 'Times New Roman Times serif',
-                            fontWeight: 'bold',
-                          }}
-                        >
-                          {perfilUser.Funcao} da Célula {perfilUser.Celula}
-                        </Box>
+                    <Grid item xs={8}>
+                      <Box
+                        ml={0}
+                        height="100%"
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                      >
+                        <Box>
+                          <Box
+                            sx={{
+                              color: '#fff',
+                              fontFamily: 'Times New Roman Times serif',
+                            }}
+                          >
+                            Funcão:
+                          </Box>
+                          <Box
+                            sx={{
+                              color: '#76ff03',
+                              fontFamily: 'Times New Roman Times serif',
+                              fontWeight: 'bold',
+                            }}
+                          >
+                            {perfilUser.Funcao} da Célula {perfilUser.Celula}
+                          </Box>
 
-                        <Box
-                          sx={{
-                            color: '#fff',
-                            fontFamily: 'Times New Roman Times serif',
-                          }}
-                        >
-                          Matricula:
-                        </Box>
-                        <Box
-                          sx={{
-                            color: '#76ff03',
-                            fontFamily: 'Times New Roman Times serif',
-                            fontWeight: 'bold',
-                          }}
-                        >
-                          {perfilUser.Igreja}
-                          {perfilUser.RolMembro}{' '}
+                          <Box
+                            sx={{
+                              color: '#fff',
+                              fontFamily: 'Times New Roman Times serif',
+                            }}
+                          >
+                            Matricula:
+                          </Box>
+                          <Box
+                            sx={{
+                              color: '#76ff03',
+                              fontFamily: 'Times New Roman Times serif',
+                              fontWeight: 'bold',
+                            }}
+                          >
+                            {perfilUser.Igreja}
+                            {perfilUser.RolMembro}{' '}
+                          </Box>
                         </Box>
                       </Box>
-                    </Box>
-                  </Grid>
-                </Box>
-              </Grid>
+                    </Grid>
+                  </Box>
+                </Grid>
+              </Box>
             </Box>
           </Box>
         </Box>
