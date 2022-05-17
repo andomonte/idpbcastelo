1617638b8ad8bf3b6070d13e56131f3ae9fa4bd7
andomonte <andomonte@gmail.com>
2022-05-16 22:03:07 -0400

    v0.15

--- src/pages/cursos/matricula.js
+++ src/pages/cursos/matricula.js
@@ -17,7 +17,7 @@ export const getStaticProps = async () => {
   const cursos = await prisma.cursos.findMany().finally(async () => {
     await prisma.$disconnect();
   });
-  const inscritosCursos = await prisma.cursosinscritos
+  const inscritosCursos = await prisma.inscritosCursos
     .findMany()
     .finally(async () => {
       await prisma.$disconnect();
